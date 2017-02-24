9d102665ba8cec0df92d3d35d16558e14b9bc3bc
kevinf <kevinf@128.4.205.165>
2017-02-24 11:53:16 -0500

    P1 P2 done

--- main.cpp
+++ main.cpp
@@ -20,33 +20,67 @@
 
 using namespace std;
 
-/*
- * 
- */
-int order1(int x, int y);
 
+bool swap1(int *x, int *y);
+bool swap2(int &x, int &y);
 int main() {
     srand(time(NULL));
-    int i;
+    int x;
+    int y;
     cout << "Hello World!\n";
-    i = (rand()%51)+1;
-    cout << i;
+    x = (rand()%51)+1;
+    y = (rand()%51)+1;
+    cout << x << endl;
+    cout << y << endl;
+    cout <<swap1(&x, &y) << endl;
+    cout <<swap2(x,y)<<endl;
+    
     
     
 
 }
-/*
-int order1(int x, int y) {
+//Problem 1
+bool swap1(int *x, int *y) {
     if (x > y) {
-        int tmp = x;
-        x = y;
-        y = tmp;
-        //make it call by pointer
+        int swap;
+        swap = *x;
+        *x = *y;
+        *y = swap;        
+        return false;
         //return false;
     }
     else{
         return true;
     }
 }
-*/
+
+
+
+//Problem 2
+bool swap2(int &x, int &y) {
+    if (x > y) {
+        int swap;
+        swap = x;
+        x = y;
+        y = swap;        
+        return false;
+        //return false;
+    }
+    else{
+        return true;
+    }
+}
+
+//Problem 5
+
+/*
+ (6 pts) Write a function that takes as an input parameter an array of integers, a length parameter (an int),
+and two more int parameters that will be modified using pass by reference. When the function is called
+the third and fourth parameters are initialized to -1 (before the function call). The function should locate
+the smallest value in the array, modifying the third parameter to be the smallest value and the fourth to
+be the index location of the smallest value. Make sure you print these values after you’ve returned from
+the function.
+ */
+
+
 
